0fa020397326f58b7c54b56abcee143a3a3d69c3
Anas-Rabeea <AnasRabeea001@gmail.com>
2026-03-09 14:36:26 +0200

    initial commit

---
+++ cinema-ticket-system/src/main/java/dev/anascoding/cinema_ticket_system/CinemaTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.anascoding.cinema_ticket_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketSystemApplication.class, args);
+	}
+
+}
---
+++ cinema-ticket-system/src/test/java/dev/anascoding/cinema_ticket_system/CinemaTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anascoding.cinema_ticket_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
